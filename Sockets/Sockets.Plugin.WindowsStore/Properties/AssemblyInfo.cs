3940bbc4dc4391c53a4b2ce0ba5d38ea9ad3db53
rdavisau <ryandavis.au@gmail.com>
2015-02-09 18:13:30 +1000

    oops, assembly version

--- Sockets/Sockets.Plugin.WindowsStore/Properties/AssemblyInfo.cs
+++ Sockets/Sockets.Plugin.WindowsStore/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.3")]
-[assembly: AssemblyFileVersion("1.1.3")]
+[assembly: AssemblyVersion("1.1.4")]
+[assembly: AssemblyFileVersion("1.1.4")]
 [assembly: ComVisible(false)]
