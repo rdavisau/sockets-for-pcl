e05d81e4b40c902ca0a6325bb8f2f71d036e29dc
NewtonARA <satoshi.ara@newton-graphics.co.jp>
2015-04-30 20:42:53 +0900

    Add MonoMac, Xamarin.Mac 1.0, Xamarin.Mac 2.0

---
+++ Sockets/Sockets.Plugin.MacOpenSource/Netmask/CommsInterfaceNative.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Sockets.Plugin
+{
+    public partial class CommsInterface
+    {
+        /// <summary>
+        /// UnicastIPAddressInformation.IPv4Mask is not implemented in Xamarin. This method sits in a partial class definition
+        /// on each native platform and retrieves the netmask in whatever way it can be done for each platform. 
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        protected static IPAddress GetSubnetMask(UnicastIPAddressInformation ip)
+        {
+            var nativeInterfaceInfo = NetInfo.GetInterfaceInfo();
+            var match = nativeInterfaceInfo.FirstOrDefault(ni => ni != null && ni.Address != null && ip != null && ip.Address != null && Equals(ni.Address, ip.Address));
+
+            return match != null ? match.Netmask : null;
+        }
+    }
+
+}
