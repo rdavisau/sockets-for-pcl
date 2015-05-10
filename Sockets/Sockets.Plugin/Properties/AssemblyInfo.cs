e219aab4c7f2f17fa08b0d1167f8f6a7250268a9
rdavisau <ryandavis.au@gmail.com>
2015-05-10 15:22:53 +1000

    update assembly v

--- Sockets/Sockets.Plugin/Properties/AssemblyInfo.cs
+++ Sockets/Sockets.Plugin/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.1.7")]
-[assembly: AssemblyFileVersion("1.1.7")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
