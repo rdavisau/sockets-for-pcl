7e04e4223648799ea04c5c683e1d3856d18f4740
Daniel Weber <dw@exram.de>
2015-06-28 15:05:44 +0200

    Add NativeExceptionExtensions class that contains a method to to automatically wrap platform socket exceptions thrown by async methods.

---
+++ Sockets/Sockets.Implementation.NET/Extensions/NativeExceptionExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using PclSocketException = Sockets.Plugin.Abstractions.SocketException;
+using PlatformSocketException = System.Net.Sockets.SocketException;
+
+namespace Sockets.Plugin.Abstractions
+{
+    public static class NativeExceptionExtensions
+    {
+        internal static readonly HashSet<Type> NativeSocketExceptions = new HashSet<Type> { typeof(PlatformSocketException) };
+
+        public static Task WrapNativeSocketExceptions(this Task task)
+        {
+            return task.ContinueWith(
+                t =>
+                {
+                    var ex = t.Exception.InnerException;
+
+                    throw (NativeExceptionExtensions.NativeSocketExceptions.Contains(ex.GetType())) 
+                        ? new PclSocketException(ex)
+                        : ex;
+                },
+                TaskContinuationOptions.OnlyOnFaulted);
+        }
+
+        public static Task<T> WrapNativeSocketExceptions<T>(this Task<T> task)
+        {
+            return task.ContinueWith(
+                t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        var ex = t.Exception.InnerException;
+
+                        throw (NativeExceptionExtensions.NativeSocketExceptions.Contains(ex.GetType()))
+                            ? new PclSocketException(ex)
+                            : ex;
+                    }
+
+                    return t.Result;
+                }, TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
