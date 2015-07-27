e4318286d8084954de58a702d43ff16909ec2528
rdavisau <ryandavis.au@gmail.com>
2015-07-27 22:50:20 +1000

    update droid test references

--- Sockets/Tests/Sockets.Tests.DroidRunner/Resources/Resource.Designer.cs
+++ Sockets/Tests/Sockets.Tests.DroidRunner/Resources/Resource.Designer.cs
@@ -26,6 +26,8 @@ namespace Sockets.Tests.DroidRunner
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Resource.String.ApplicationName = global::Sockets.Tests.DroidRunner.Resource.String.ApplicationName;
+			global::Xamarin.Forms.Platform.Resource.String.Hello = global::Sockets.Tests.DroidRunner.Resource.String.Hello;
 		}
 		
 		public partial class Attribute
