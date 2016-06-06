0bfc468bf99579805df68997401210c8863c06b0
rdavisau <ryandavis.au@gmail.com>
2016-06-06 21:03:19 +1000

    2.0.1-pre

--- Sockets/Sockets.Plugin.MacOpenSource/Properties/AssemblyInfo.cs
+++ Sockets/Sockets.Plugin.MacOpenSource/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyFileVersion("2.0.1")]
